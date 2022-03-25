f8210680923de5f180b27206316aaf575b00ad09
Talaibekov Erkinbek <32312803+ERERkin@users.noreply.github.com>
2022-03-25 19:55:25 +0600

    Add Adapter module

---
+++ src/main/java/kg/erking/designpatterns/creationalPatterns/adapter/Adapter.java
@@ -0,0 +1,7 @@
+package kg.erking.designpatterns.creationalPatterns.adapter;
+
+public class Adapter {
+    public void test(){
+
+    }
+}
---
+++ src/test/java/kg/erking/designpatterns/creationalPatterns/adapter/AdapterTest.java
@@ -0,0 +1,12 @@
+package kg.erking.designpatterns.creationalPatterns.adapter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdapterTest {
+
+    @Test
+    void test1() {
+    }
+}
