84adf0618de314a3fae52c7bccf92d121ece91d5
Talaibekov Erkinbek <32312803+ERERkin@users.noreply.github.com>
2022-03-28 23:36:43 +0600

    Add Bridge module

---
+++ src/main/java/kg/erking/designpatterns/structuralPatterns/bridge/Bridge.java
@@ -0,0 +1,7 @@
+package kg.erking.designpatterns.structuralPatterns.bridge;
+
+public class Bridge {
+    public void test(){
+
+    }
+}
---
+++ src/test/java/kg/erking/designpatterns/structuralPatterns/bridge/BridgeTest.java
@@ -0,0 +1,12 @@
+package kg.erking.designpatterns.structuralPatterns.bridge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeTest {
+
+    @Test
+    void test1() {
+    }
+}
